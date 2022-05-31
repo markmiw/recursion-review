3cbb0d1a7cd8143cef1ae743e851d6f18c40baf1
markmiw <markmiw6@gmail.com>
2022-05-31 12:21:14 -1000

    pair partner #1

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,21 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var list = [];
+  var body = document.body;
+  var checkBody = function(body) {
+    if (body.classList && body.classList.contains(className)) {
+      list.push(body);
+    }
+    if (body.childNodes) {
+      for (var i = 0; i < body.childNodes.length; i++) {
+        checkBody(body.childNodes[i]);
+      }
+    }
+  };
+  checkBody(body);
+  return list;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,30 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
-
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
   // your code goes here
+  var objCheck = [];
+  var arrayCheck = [];
+  if (typeof obj === 'number') {
+    return obj.toString();
+  } else if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'boolean') {
+    return obj ? 'true' : 'false';
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj) === true) {
+    obj.forEach(function(item) {
+      arrayCheck.push(stringifyJSON(item));
+    });
+    return '[' + arrayCheck + ']';
+  } else if (typeof obj === 'object') {
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+        continue;
+      }
+      objCheck.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    return '{' + objCheck + '}';
+  }
 };
